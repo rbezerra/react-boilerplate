7f4c4d49e907aaf9e057b7c9cb2c2a92fb3df79e
Ramon Bezerra <ramon.bezerra@huufma.br>
2019-04-03 16:26:51 -0300

    configure react-router

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
-import store from "./store";
-import App from "./components/App";
+import store from './store';
+import App from './components/App';
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
-    document.getElementById("root")
+    <BrowserRouter>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </BrowserRouter>,
+    document.getElementById('root')
 );
